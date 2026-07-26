58f5e0eaa23886be2f29e01026a7f724d69b770a
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-07-26 05:17:41 +0000

    feat(settings): route dashboard writes through daemon

--- src/dashboard/contract_schema.rs
+++ src/dashboard/contract_schema.rs
@@ -15,6 +15,7 @@ use super::graph_structure_api::{
     StrataMeasurementV1, StructureReadV1, TestMapMeasurementV1,
 };
 use super::read_model::{DASHBOARD_SCHEMA_REVISION_V1, DashboardEnvelopeV1};
+use super::settings_api::{ProjectSettingsPatch, SettingsPayloadV1, UserSettingsPatch};
 use super::storage_findings_api::StorageFindingsPayloadV1;
 use super::storage_telemetry_api::StorageTelemetryPayloadV1;
 
@@ -30,6 +31,9 @@ struct DashboardContractCatalogV1 {
     doctor_remediation: DoctorRemediationPayloadV1,
     explorer_query_run: ExplorerQueryRunV1,
     code_index_freshness: CodeIndexFreshnessPayloadV1,
+    settings: SettingsPayloadV1,
+    settings_project_patch: ProjectSettingsPatch,
+    settings_user_patch: UserSettingsPatch,
     graph_call_chain: StructureReadV1<CallChainMeasurementV1>,
     graph_strata: StructureReadV1<StrataMeasurementV1>,
     graph_fact_matches: StructureReadV1<FactMatchesMeasurementV1>,
