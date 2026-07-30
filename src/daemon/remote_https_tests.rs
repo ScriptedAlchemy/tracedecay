bb9674d9a2360bbec962cd14f5053da64bdae24f
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-07-30 19:37:24 +0000

    test(remote): reject plaintext listener advertisement

--- src/daemon/remote_https_tests.rs
+++ src/daemon/remote_https_tests.rs
@@ -1,4 +1,7 @@
-use super::remote_https::{RemoteBrainHttpsConfigV1, RemoteBrainHttpsStateV1};
+use super::remote_https::{
+    RemoteBrainHttpsConfigV1, RemoteBrainHttpsEnablementV1, RemoteBrainHttpsError,
+    RemoteBrainHttpsStateV1,
+};
 
 #[test]
 fn remote_brain_https_is_unconfigured_by_default() {
@@ -6,4 +9,21 @@ fn remote_brain_https_is_unconfigured_by_default() {
 
     assert_eq!(config.version, 1);
     assert_eq!(config.state(), RemoteBrainHttpsStateV1::Unconfigured);
+}
+
+#[test]
+fn enabled_remote_brain_listener_rejects_plaintext_advertised_endpoint() {
+    let config = RemoteBrainHttpsConfigV1 {
+        enablement: RemoteBrainHttpsEnablementV1::Enabled,
+        bind_address: Some("127.0.0.1:0".to_owned()),
+        advertised_endpoint: Some("http://remote.example".to_owned()),
+        certificate_chain_path: Some("test-cert.pem".into()),
+        private_key_path: Some("test-key.pem".into()),
+        ..RemoteBrainHttpsConfigV1::default()
+    };
+
+    assert!(matches!(
+        config.validate_enabled(),
+        Err(RemoteBrainHttpsError::InvalidAdvertisedEndpoint)
+    ));
 }
