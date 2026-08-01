9ca13c838f9c796f49e479da994c907645091d3d
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-08-01 16:14:09 +0000

    chore: delete orphaned root modules

--- src/lib.rs
+++ src/lib.rs
@@ -71,7 +71,6 @@ pub mod dashboard;
 #[path = "../build-support/dashboard_cache.rs"]
 mod dashboard_build_cache;
 #[cfg(test)]
-mod dashboard_diagnostics;
 pub mod db;
 mod dependency_imports;
 pub mod derive_table;
