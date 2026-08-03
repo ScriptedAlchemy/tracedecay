3cf86e6db5a6e14bdf6255c491444de5687d0378
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2026-08-03 17:56:17 +0000

    fix(sessions): drop obsolete git facade export

--- src/git.rs
+++ src/git.rs
@@ -1,4 +1,4 @@
 //! Compatibility facade for low-level git process helpers.
 
 pub use tracedecay_runtime_core::git::git_program;
-pub(crate) use tracedecay_runtime_core::git::{git_capture, git_output};
+pub(crate) use tracedecay_runtime_core::git::git_capture;
